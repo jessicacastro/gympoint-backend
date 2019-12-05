d0919bbe8fe0cc4574958ca5ad9350946134508e
Jessica Castro <jessica.castros@outlook.com>
2019-12-05 19:58:37 -0300

    Modificação em seeds e create student table

---
+++ src/database/migrations/20191205225327-create-students.js
@@ -0,0 +1,33 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('students', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      weight: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      height: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('students');
+  },
+};
